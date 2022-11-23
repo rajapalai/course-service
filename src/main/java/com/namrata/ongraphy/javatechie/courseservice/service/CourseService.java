3f27c7e5c729514231862abe8f25c75e8a945dbb
namrata.chowdhury05@gmail.com <namrata.chowdhury05@gmail.com>
2022-11-23 16:50:53 +0530

    saving code

--- src/main/java/com/namrata/ongraphy/javatechie/courseservice/service/CourseService.java
+++ src/main/java/com/namrata/ongraphy/javatechie/courseservice/service/CourseService.java
@@ -1,45 +1,80 @@
 package com.namrata.ongraphy.javatechie.courseservice.service;
 
-
-import com.namrata.ongraphy.javatechie.courseservice.dto.Course;
+import com.namrata.ongraphy.javatechie.courseservice.dao.CourseDAO;
+import com.namrata.ongraphy.javatechie.courseservice.dto.CourseRequestDTO;
+import com.namrata.ongraphy.javatechie.courseservice.dto.CourseResponseDTO;
+import com.namrata.ongraphy.javatechie.courseservice.entity.CourseEntity;
+import com.namrata.ongraphy.javatechie.courseservice.util.AppUtils;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
-public class CourseService {
+@AllArgsConstructor
 
-    private List<Course> courseDatabase = new ArrayList<>();
+public class CourseService {
+    private CourseDAO courseDAO;
+
+    // private List<Course> courseDatabase = new ArrayList<>();
+    //we do not need ArrayList as we will use DB now
 
     //create course object in DB--post
-    public Course onBoardNewCourse(Course course){
-        course.setCourseId(new Random().nextInt(3756));
-        courseDatabase.add(course);
-         return course;
+    public CourseResponseDTO onBoardNewCourse(CourseRequestDTO courseRequestDTO){
+
+        //Convert DTO to entity
+        CourseEntity courseEntity = AppUtils.mapDTOToEntity(courseRequestDTO);
+        CourseEntity save = courseDAO.save(courseEntity);
+        //convert entity->response DTO
+        CourseResponseDTO courseResponseDTO = AppUtils.mapEntityToDTO(save);
+        courseResponseDTO.setCourseUniqueCode(UUID.randomUUID().toString().split("-")[0]);
+        return courseResponseDTO;
     }
 
     //load all the course from database---get
-    public List<Course> viewAllCourse(){
-        return courseDatabase;
+    public List<CourseResponseDTO> viewAllCourse(){
+
+        Iterable<CourseEntity> courseEntities = courseDAO.findAll();
+        //lambda expression
+        /*return StreamSupport.stream(courseEntities.spliterator(),false)
+                .map(courseEntity -> AppUtils.mapEntityToDTO(courseEntity)).collect(Collectors.toList());*/
+             //Method reference
+        return StreamSupport.stream(courseEntities.spliterator(),false)
+                .map(AppUtils::mapEntityToDTO).collect(Collectors.toList());
+
     }
     //filter course bu courseId---get
-    public Course findByCourseId(Integer courseId){
-          return courseDatabase.stream().filter(course->course.getCourseId()==courseId).findFirst().orElse(null);
+    public CourseResponseDTO findByCourseId(Integer courseId){
+        CourseEntity courseEntity = courseDAO.findById(courseId)
+                .orElseThrow(() -> new RuntimeException(courseId + "Not valid"));
+        return AppUtils.mapEntityToDTO(courseEntity);
     }
 
     //delete course---delete
     public void deleteCourse(int courseId){
-        Course course = findByCourseId(courseId);
-         courseDatabase.remove(course);
+       courseDAO.deleteById(courseId);
     }
 
     //update the course--put or patch
-    public Course updateCourse(int courseId,Course course){
-        Course existingCourse = findByCourseId(courseId);
-        courseDatabase.set(courseDatabase.indexOf(existingCourse),course);
-        return course;
+    public CourseResponseDTO updateCourse(int courseId,CourseRequestDTO courseRequestDTO){
+
+        //get the existing object
+        CourseEntity existingCourseEntity = courseDAO.findById(courseId).orElse(null);
+        //modify existing with new value then
+        existingCourseEntity.setName(courseRequestDTO.getName());
+        existingCourseEntity.setTrainerName(courseRequestDTO.getTrainerName());
+        existingCourseEntity.setDuration(courseRequestDTO.getDuration());
+        existingCourseEntity.setStartDate(courseRequestDTO.getStartDate());
+        existingCourseEntity.setCourseType(courseRequestDTO.getCourseType());
+        existingCourseEntity.setFees(courseRequestDTO.getFees());
+        existingCourseEntity.setCertificateAvailable(courseRequestDTO.isCertificateAvailable());
+        existingCourseEntity.setDescription(courseRequestDTO.getDescription());
+           // save modified object
+        CourseEntity updatedCourseEntity = courseDAO.save(existingCourseEntity);
+        return AppUtils.mapEntityToDTO(updatedCourseEntity) ;
 
     }
 
